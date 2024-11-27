7838ac8ea6454780fc955b5d253130bc2f1091f8
Andrea-Duke-Abaidoo <andreadukeabaidoo@gmail.com>
2024-11-27 11:14:03 +0000

    added index.js hw1

---
+++ index.js
@@ -0,0 +1,6 @@
+alert("Hello Elpea");
+let name = prompt("What is your name");
+let age = prompt("How old are you");
+let email = prompt("What's your e-mail");
+
+alert(`hi ${name}, you are ${age} years old and we will keep in touch via your ${email}:`)
